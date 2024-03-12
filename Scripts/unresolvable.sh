5b81afc6ca813aed2b7d973f085f8c69aac6991b
SquallForger24 <159946529+SquallForger24@users.noreply.github.com>
2024-03-12 13:14:50 -0700

    Update unresolvable.sh

--- Scripts/unresolvable.sh
+++ Scripts/unresolvable.sh
@@ -3,8 +3,8 @@
 fqdn=$1
 
 if [ -n "$(dig +short $fqdn)" ]; then
-	echo "$fqdn resolves externally"
+	echo "$fqdn resolves"
 else
-	echo "$fqdn does not resolve externally!"
+	echo "$fqdn does not resolve"
 	echo $fqdn >> unresolvable.txt
 fi
